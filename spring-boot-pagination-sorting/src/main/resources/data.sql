0c4fda1e50c91d298f9157d970f533e6dc37ac46
vskreddy652 <vskreddy652@yahoo.com>
2021-08-19 09:02:25 +0530

    Add files via upload

---
+++ spring-boot-pagination-sorting/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO 
+	TBL_EMPLOYEES (first_name, last_name, email) 
+VALUES
+  	('name11', 'lname1', 'lname@gmail.com'),
+('name2', 'lname2', 'lname@gmail.com'),
+('name3', 'lname3', 'lname@gmail.com'),
+('name4', 'lname4', 'lname@gmail.com'),
+('name5', 'lname5', 'lname@gmail.com'),
+('name6', 'lname6', 'lname@gmail.com'),
+  	('name8', 'lname', 'xyz@email.com');
